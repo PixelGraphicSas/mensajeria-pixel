dc040c38082e2ecb02843436349dfb4cff5ffdb6
PixelGraphicSas <gerencia@pixel-g.com>
2026-06-09 15:18:01 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js — NO subir a GitHub (agregar a .gitignore)
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBlRox25xkQiKJhNImXeH93jfxQiqUh_BQ",
+  authDomain: "mensajeria-pixel.firebaseapp.com",
+  projectId: "mensajeria-pixel",
+  storageBucket: "mensajeria-pixel.firebasestorage.app",
+  messagingSenderId: "834377097535",
+  appId: "1:834377097535:web:457fe79c1db8713cbaf38a"
+};
+
+// PIN del mensajero (el admin puede cambiarlo aquí)
+const MENSAJERO_PIN = "1234";
+
+// PIN del administrador
+const ADMIN_PIN = "0000";
